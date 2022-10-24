a417f23f26447e17fe118b16a40ca6797ba37bd0
swarnim38 <swarnim.marathe@gmail.com>
2022-10-25 02:51:24 +0530

    Learnt about data types

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        int age = 30;
+        Date now = new Date();
         System.out.println("Hello World");
+        System.out.println(age);
+        System.out.println(now);
     }
 }
